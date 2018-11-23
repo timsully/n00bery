1327d91816c9b1326620cdd552437469812c9086
Tim Sullivan <timsullivan97@gmail.com>
2018-11-22 21:18:14 -0800

    adding ipcRenderer

--- electron-metadata/src/renderer.js
+++ electron-metadata/src/renderer.js
@@ -6,6 +6,26 @@ const submitListener = document
         event.preventDefault()
 
         // an array of files with some metadata
-        const files = document.getElementById('filePicker').files
-        console.log(files);
+        const files = [...document.getElementById('filePicker').files]
+    
+        // format the file data to only path and name
+        const filesFormatted = files.map({ name, path: pathName } => ({
+            name,
+            pathName
+        }))
+
+        // send the data to the main process
+        ipcRenderer.send('files', filesFormatted)
+    })
+
+    // metadata from the main process
+    ipcRenderer.on('metadata', (event, metadata) {
+        const pre = document.getElementById('data')
+
+        pre.innerText = JSON.stringify(metadata, null, 2)
+    })
+
+    // error event from catch block in main process
+    ipcRenderer.on('metadata:error', (event, error) => {
+        console.error(error)
     })
