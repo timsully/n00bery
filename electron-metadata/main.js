32f7d9ea54912b1fb872d9a552392778336c627c
Tim Sullivan <timsullivan97@gmail.com>
2018-11-22 16:57:00 -0800

    add path module

--- electron-metadata/main.js
+++ electron-metadata/main.js
@@ -3,3 +3,18 @@ const { app, BrowserWindow } = require('electron')
 app.on('ready', () => {
     console.log('app ready');
 })
+
+const path = require('path')
+
+// declare this as a variable globally so we can
+// reference it and so it will not be garbage collected
+let mainWindow
+
+// wait for the main process to be ready
+app.on('ready', () => {
+    // path to our html
+    const htmlPath = path.join('src', 'index.html')
+
+    // load the html file
+    mainWindow.loadFile(htmlPath)
+})
