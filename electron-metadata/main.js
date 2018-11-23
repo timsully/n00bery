d04c7e9a7a257a03a85d856fb2befb5e99d85f1f
Tim Sullivan <timsullivan97@gmail.com>
2018-11-22 21:26:00 -0800

    listen for files event by browser process

--- electron-metadata/main.js
+++ electron-metadata/main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron')
+const { ipcMain } = require('electron')
 
 app.on('ready', () => {
     console.log('app ready');
@@ -20,4 +21,31 @@ app.on('ready', () => {
 
     // load the html file
     mainWindow.loadFile(htmlPath)
+})
+
+// load util and fs from node
+const util = require('util')
+const fs = require('fs')
+
+// make fs.stat able to use promises ( instead of callbacks )
+const stat = util.promisify(fs.stat)
+
+ipcMain.on('files', async (event, filesArr) => {
+    try {
+        // asynchronously get the data for all the files
+        const data = await Promise.all(
+            filesArr.map(async ({ name, pathName }) => ({
+                ...await stat(pathName),
+                name,
+                pathName
+            }))
+        )
+
+        // remember we declared mainWindow ourselves
+        // when we created a new browser window
+        mainWindow.webContents.send('metadata', data)
+    } catch (error) {
+        // send an error event if something goes wrong
+        mainWindow.webContents.send('metadata:error', error)
+    }
 })
